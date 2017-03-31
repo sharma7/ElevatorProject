7219eec3e20ea35f07c24c879aec3643dc33a26f
gobingiri <gobingiritharan@gmail.com>
2017-03-31 00:17:39 -0400

    latest version of our incomplete FSM

--- elevatorfsm.v
+++ elevatorfsm.v
@@ -1,44 +1,50 @@
 module elevatorfsm(clk, clr_, w);
-	
-	input clk, clr_;
-    
+
+    input clk, clr_;
+
     // The input for floors, is a 3 bit input
-    input [2:0]	w;
-    
+    input [2:0] w;
+
     // Represents current and new states
-	reg [2:0] y, Y;
-	
-	// Each of our states for the floors of elevator
-	// Ground Floor, Floor 2, Floor 3, Floor 4
-	localparam A=3'b001, B=3'b010, C=3'b011, D=3'b100;
-	
-	// check when current state changes or when a variable changes
-	always @(y,w)
-		case (y)
-			// Elevator Logic
-			A:  if (w == A) Y = A;
-				else if (w == B) Y = B;
-			 	else if (w == C) Y = C;
-				else Y = D;
-			B:  if (w == A) Y = A;
-				else if (w == B) Y = B;
-				else if (w == C) Y = C;
-				else Y = D;
-			C:  if (w == A) Y = A;
-				else if (w == B) Y = B;
-				else if (w == C) Y = C;
-				else Y = D;
-			D:  if (w == A) Y = A;
-				else if (w == B) Y = B;
-				else if (w == C) Y = C;
-				else Y = D;
-				
-			default: Y = A;
-		endcase
-	
-	// Does the work of changing current state (y) to new state (Y)	
-	always @(negedge clr_,  posedge clk)
-		if (!clr_) y <= A;
-		else y<= Y;
+    reg [2:0] y, Y;
+
+    // Each of our states for the floors of elevator
+    // Ground Floor, Floor 2, Floor 3, Floor 4
+    localparam A=3'b001, B=3'b010, C=3'b011, D=3'b100;
+    always @(w,y)
+        case (y)
+            A: if (w == A) Y = A;
+               else if (w == B)
+                    begin
+                        stop = 1'b0;
+                        direction = 1'b1;
+                        start = 1'b1;
+                        if (count == 4'b0011)
+                            begin
+                                stop = 1'b1;
+                                start = 1'b0;
+                                Y = B;
+                            end
+                    end
+                else if (w == C) Y = C;
+                else Y = D;
+            B: if (w == A) Y = A;
+               else if (w == B) Y = B;
+               else if (w == C) Y = C;
+               else Y = D;
+            C: if (w == A) Y = A;
+               else if (w == B) Y = B;
+               else if (w == C) Y = C;
+               else Y = D;
+            D: if (w == A) Y = A;
+               else if (w == B) Y = B;
+               else if (w == C) Y = C;
+               else Y = D;
+            default: Y = A;
+    endcase
+
+always @(negedge SW[3],  posedge CLOCK_50)
+    if (!SW[3]) y <= A;
+    else y<= Y;
 
 endmodule
